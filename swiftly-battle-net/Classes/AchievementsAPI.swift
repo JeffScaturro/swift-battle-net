8d4e0b90c652ad0e02a84285c9b1a864ed9db463
Jeff Scaturro <jscaturro@asu.edu>
2016-06-07 22:16:49 -0700

    Updating travis.yml to allow warnings.

--- swiftly-battle-net/Classes/AchievementsAPI.swift
+++ swiftly-battle-net/Classes/AchievementsAPI.swift
@@ -1,5 +1,5 @@
 //
-//  AchievementsRouter.swift
+//  AchievementsAPI.swift
 //  swiftly-battle-net
 //
 //  Created by Jeffrey Scaturro on 6/7/16.
@@ -10,23 +10,23 @@ import Alamofire
 
 internal class AchievementsAPI {
     let baseURLString = NSURL(string: "https://us.api.battle.net/wow/achievement/")
-    
+
     // TODO: Return an object of type Achievement (yet to be constructed).
     public func getAchievement(id: String) -> NSMutableURLRequest {
         let url: NSURL = {
             let url = NSURL(string: "\(id)?locale=\(APIManager.sharedInstance.locale)&apikey=\(APIManager.sharedInstance.apiKey)",
                             relativeToURL:baseURLString)!
-            
+
             return url
         }()
-        
+
         let URLRequest = NSMutableURLRequest(URL: url)
-        
+
         let encoding = Alamofire.ParameterEncoding.JSON
         let (encodedRequest, _) = encoding.encode(URLRequest, parameters: nil)
-        
+
         encodedRequest.HTTPMethod = Alamofire.Method.GET.rawValue
-        
+
         return encodedRequest
     }
 }
